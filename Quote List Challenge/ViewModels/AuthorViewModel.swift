889d5eda8b8d7df3e2845a243a0e9d78bd1a580b
Grant Christensen <59584917+grantpc@users.noreply.github.com>
2022-10-15 13:39:45 -0600

    Added AuthorViewModel to the project

---
+++ Quote List Challenge/ViewModels/AuthorViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  AuthorViewModel.swift
+//  Quote List Challenge
+//
+//  Created by Grant Christensen on 10/15/22.
+//
+
+import Foundation
+
+struct Author {
+    
+    var name:String?
+    var favorited:Bool?
+    
+}
