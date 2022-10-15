a32a22fc1f4d90667ac685bdcb0f386beea91c89
Grant Christensen <59584917+grantpc@users.noreply.github.com>
2022-10-15 10:10:25 -0600

    Initial Commit

---
+++ Quote List Challenge/Quote_List_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_List_ChallengeApp.swift
+//  Quote List Challenge
+//
+//  Created by Grant Christensen on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Quote_List_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
